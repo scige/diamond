b02dd0924872ec1905c93007b75e1111f05d555e
scige <panf06@gmail.com>
2013-06-25 23:01:16 +0800

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diamond::Application.config.secret_token = '66f1abd662292c35092314bef6a1f129f9f98418336cc73ffa10a7b8f91a1e69120916576661a3f139be77a0536a8c0cb1c421194dd473221c69aa2921f7f3da'
