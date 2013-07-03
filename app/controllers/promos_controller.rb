1963d062db784e20933c99661e4dbbd20e546757
scige <panf06@gmail.com>
2013-07-03 22:58:23 +0800

    add promos/show feature

--- app/controllers/promos_controller.rb
+++ app/controllers/promos_controller.rb
@@ -1,2 +1,7 @@
 class PromosController < ApplicationController
+  layout "application_mobile"
+
+  def show
+    @promo = Promo.find_by_id(params[:id])
+  end
 end
--- app/helpers/promos_helper.rb
+++ app/helpers/promos_helper.rb
@@ -1,2 +1,5 @@
 module PromosHelper
+  def get_format_date(time)
+    time != nil ? time.strftime('%Y-%m-%d') : ""
+  end
 end
