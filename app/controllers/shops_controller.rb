51dd409921f693fd9d4ba16d3e7713974fce9ca0
scige <panf06@gmail.com>
2013-07-09 21:40:36 +0800

    modify weixin view shop, but css neet to simplify

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -4,5 +4,24 @@ class ShopsController < ApplicationController
   def show
     @shop = Shop.find_by_id(params[:id])
     @guid = params[:spm]
+    part = @shop.recommended_products.split
+    @recommended_products = []
+    cur = 0
+    item = []
+    part.each do |p|
+      cur += 1
+      item << p
+      if cur == 4
+        @recommended_products << item
+        cur = 0
+        item = []
+      end
+    end
+    if item.size > 0
+      (1..(4-item.size)).each do |i|
+        item << ""
+      end
+      @recommended_products << item
+    end
   end
 end
