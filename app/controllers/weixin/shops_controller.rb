ba7fdff9929217d3b382831ee0249613275c5032
scige <panf06@gmail.com>
2013-07-11 14:54:36 +0800

    1.search shop by name and navigation; 2.add welcome infomation

--- app/controllers/weixin/shops_controller.rb
+++ app/controllers/weixin/shops_controller.rb
@@ -2,8 +2,15 @@ class Weixin::ShopsController < Weixin::ApplicationController
   before_filter :sync_weixin_user_status
 
   def index
-    content = params[:xml][:Content][1..-1]
-    @shops = Shop.where("name like '%s'" % "%#{content}%").limit(3)
-    render "index"
+    @content = params[:xml][:Content]
+    @shops = Shop.where("name like '%s'" % "%#{@content}%").limit(6)
+    if @shops.size < 6
+      @shops += Shop.where("navigation like '%s'" % "%#{@content}%").limit(6-@shops.size)
+    end
+    if @shops.size == 0
+      render "weixin/shared/noresult"
+    else
+      render "index"
+    end
   end
 end
