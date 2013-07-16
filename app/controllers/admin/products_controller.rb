623ca22a7365f164768e8cb7980416ee064fafe8
scige <panf06@gmail.com>
2013-07-16 22:28:55 +0800

    1.destory product feature finish; 2.edit product feature need javascript process

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,22 +3,6 @@
 class Admin::ProductsController < ApplicationController
   before_filter :authenticate_super!
 
-  def index
-    @products = Product.all
-  end
-
-  def show
-    @product = Product.find(params[:id])
-  end
-
-  def new
-    @product = Product.new
-  end
-
-  def edit
-    @product = Product.find(params[:id])
-  end
-
   def create
     @product = Product.new(params[:product])
     @shop = Shop.find(params[:shop_id])
@@ -39,18 +23,27 @@ class Admin::ProductsController < ApplicationController
 
   def update
     @product = Product.find(params[:id])
+    @shop = Shop.find(params[:shop_id])
+    if !@shop
+      #render :text => "商家不存在。"
+      return
+    end
+
+    @product.shop = @shop
 
     if @product.update_attributes(params[:product])
-      redirect_to admin_product_url(@product)
+      flash[:info] = "修改商品成功。"
     else
-      render action: "edit"
+      flash[:error] = "修改商品失败！"
     end
+    redirect_to admin_shop_url(@product.shop)
   end
 
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
 
-    redirect_to admin_products_url
+    flash[:info] = "删除商品成功。"
+    redirect_to admin_shop_url(@product.shop)
   end
 end
