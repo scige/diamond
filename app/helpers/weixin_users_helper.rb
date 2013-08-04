c152190a27289c790a2635b55201d4330d53875c
scige <panf06@gmail.com>
2013-08-04 21:43:38 +0800

    modify weixin_users/myhome style

--- app/helpers/weixin_users_helper.rb
+++ app/helpers/weixin_users_helper.rb
@@ -1,2 +1,13 @@
 module WeixinUsersHelper
+  def has_promo?(weixin_user)
+    if weixin_user.shops.size > 0
+      has = false
+      weixin_user.shops.each do |shop|
+        if shop.promos.size > 0
+          return true
+        end
+      end
+    end
+    return false
+  end
 end
