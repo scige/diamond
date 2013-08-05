28e0e801a738aca1e6b9ae97c1427249828f1eb1
scige <panf06@gmail.com>
2013-08-05 22:57:03 +0800

    modify editor statistics finish

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,28 +1,38 @@
 class HomeController < ApplicationController
   def index
-    shops_editor1 = Shop.where("editor='846713848@qq.com'").order("created_at")
-    shops_editor2 = Shop.where("editor='469909985@qq.com'").order("created_at")
+    @shops_editor1 = Shop.where("editor='846713848@qq.com'").order("created_at")
+    @shops_editor2 = Shop.where("editor='469909985@qq.com'").order("created_at")
 
-    @count_editor1 = {}
-    shops_editor1.each do |shop|
+    @create_count_editor1 = {}
+    @working_time_editor1 = {}
+    @shops_editor1.each do |shop|
       hash_key = shop.created_at.to_s[0..9]
-      if @count_editor1.has_key?(hash_key)
-        @count_editor1[hash_key] += 1
+      if @create_count_editor1.has_key?(hash_key)
+        @create_count_editor1[hash_key] += 1
+        cur_time = shop.created_at.to_s[11..18]
+        @working_time_editor1[hash_key][1] = cur_time
       else
-        @count_editor1[hash_key] = 1
+        @create_count_editor1[hash_key] = 1
+        cur_time = shop.created_at.to_s[11..18]
+        @working_time_editor1[hash_key] = [cur_time, cur_time]
       end
     end
 
-    @count_editor2 = {}
-    shops_editor2.each do |shop|
+    @create_count_editor2 = {}
+    @working_time_editor2 = {}
+    @shops_editor2.each do |shop|
       hash_key = shop.created_at.to_s[0..9]
-      if @count_editor2.has_key?(hash_key)
-        @count_editor2[hash_key] += 1
+      if @create_count_editor2.has_key?(hash_key)
+        @create_count_editor2[hash_key] += 1
+        cur_time = shop.created_at.to_s[11..18]
+        @working_time_editor2[hash_key][1] = cur_time
       else
-        @count_editor2[hash_key] = 1
+        @create_count_editor2[hash_key] = 1
+        cur_time = shop.created_at.to_s[11..18]
+        @working_time_editor2[hash_key] = [cur_time, cur_time]
       end
     end
 
-    @date_range = "2013-07-20".to_datetime..Time.now.to_datetime
+    @date_range = "2013-07-26".to_datetime..Time.now.to_datetime
   end
 end
