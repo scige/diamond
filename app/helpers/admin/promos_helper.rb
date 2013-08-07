7de3cea80747ae2c87eb17ebba19b360607a66b0
scige <panf06@gmail.com>
2013-08-07 21:11:00 +0800

    modify status edit by select_form

--- app/helpers/admin/promos_helper.rb
+++ app/helpers/admin/promos_helper.rb
@@ -1,2 +1,7 @@
+# coding: utf-8
+
 module Admin::PromosHelper
+  def generate_promo_status
+    [["已上架",Setting.promo.status_off_shelf], ["未上架",Setting.promo.status_on_shelf], ["待审核",Setting.promo.status_not_verify]]
+  end
 end
--- app/helpers/admin/shops_helper.rb
+++ app/helpers/admin/shops_helper.rb
@@ -1,2 +1,7 @@
+# coding: utf-8
+
 module Admin::ShopsHelper
+  def generate_shop_status
+    [["已上架",Setting.shop.status_off_shelf], ["未上架",Setting.shop.status_on_shelf], ["待审核",Setting.shop.status_not_verify]]
+  end
 end
